8ed2ef004788a67391d26af12dddf9b169abd92c
UffeMan <92825162+UffeMan@users.noreply.github.com>
2022-04-28 14:06:24 +0200

    First commit

---
+++ src/main/java/com/example/backend_cleaningsupplie/BackendCleaningSupplieApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_cleaningsupplie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCleaningSupplieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCleaningSupplieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_cleaningsupplie/BackendCleaningSupplieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_cleaningsupplie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCleaningSupplieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
