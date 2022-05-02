60b0acd8afff213901f3726570474d44028ceb96
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-05-02 11:03:39 +0200

    added repo and serics classes for Companies

--- src/main/java/com/example/backend_cleaningsupplie/BackendCleaningSupplieApplication.java
+++ src/main/java/com/example/backend_cleaningsupplie/BackendCleaningSupplieApplication.java
@@ -1,5 +1,6 @@
 package com.example.backend_cleaningsupplie;
 
+import com.example.backend_cleaningsupplie.entities.Companies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class BackendCleaningSupplieApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendCleaningSupplieApplication.class, args);
     }
+
 
 }
---
+++ src/main/java/com/example/backend_cleaningsupplie/controller/CompaniesController.java
@@ -0,0 +1,5 @@
+package com.example.backend_cleaningsupplie.controller;
+
+
+public class CompaniesController {
+}
