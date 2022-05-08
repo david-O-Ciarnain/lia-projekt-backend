43b399713f7c16a3283ad87aec30014790870074
UffeMan <92825162+UffeMan@users.noreply.github.com>
2022-05-08 18:22:20 +0200

    Mailserver + lite mail stuff

--- src/main/java/com/example/backend_cleaningsupplie/email/MailSender.java
+++ src/main/java/com/example/backend_cleaningsupplie/email/MailSender.java
@@ -3,5 +3,5 @@ package com.example.backend_cleaningsupplie.email;
 import java.util.List;
 
 public interface MailSender {
-    void send(List<String> allUserMail, String subjekt, String text);
+    void send(List<String> allUserMail, String subject, String text);
 }
